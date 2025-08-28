b2b2779edbcda34dab060de5fbff9d672eb3f726
Charles Lee <charles.lee3@griffithuni.edu.au>
2025-08-28 21:41:42 +1000

    version 14.38-17.8
    Added MSVP version 14.38-17.8 and added new C++ script

---
+++ Source/Pulsefire_Nova/MyFirstCPPClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PULSEFIRE_NOVA_API MyFirstCPPClass
+{
+public:
+	MyFirstCPPClass();
+	~MyFirstCPPClass();
+};
